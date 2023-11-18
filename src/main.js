10a40716d75768d02ef84fbe1d405e80729c15ca
frederick sallah <fredysallah@gmail.com>
2023-11-18 14:31:00 +0000

    hmm

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import router from './router'
 
 const app = createApp(App)
 
-app.config.globalProperties.api_url = 'http://localhost:3000';
+//app.config.globalProperties.api_url = 'http://localhost:3000';
 
-//app.config.globalProperties.api_url = 'http://ec2-13-40-188-171.eu-west-2.compute.amazonaws.com:3000';
+app.config.globalProperties.api_url = 'http://ec2-13-40-188-171.eu-west-2.compute.amazonaws.com:3000';
 
 app.use(router)
 
